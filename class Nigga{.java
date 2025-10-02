b5d382bae283ea4ac54afb28769aa91b2d60284b
n3s4r <141773812+n3s4r@users.noreply.github.com>
2025-10-03 00:05:56 +0600

    Add files via upload

---
+++ class Nigga{.java
@@ -0,0 +1,9 @@
+class Nigga{
+    public static void main(String[] args){
+        System.out.print("Hello Nigga you are ");
+        int age = 25;
+        System.out.print(age);
+        System.out.print(" years old");
+
+    }
+}
