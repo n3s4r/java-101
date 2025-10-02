870294042ca017cf04de148aa90675adcadc0f82
n3s4r <141773812+n3s4r@users.noreply.github.com>
2025-10-03 00:48:54 +0600

    Add files via upload

---
+++ UpdatedNigga.java
@@ -0,0 +1,61 @@
+class Nigga{
+    public static void main(String[] args){
+        /*
+
+        //printing
+        System.out.print("Hello Nigga you are ");
+        int age = 25;
+        System.out.print(age);
+        System.out.print(" years old");
+        System.out.print("\n");
+        
+        //implicit conversion
+        int num1 = 5;
+        double num2 = num1;
+        System.out.println(num1);
+        System.out.println(num2);
+
+        //explicit or typecasting
+        double num3 = 6.8;
+        int num4 = (int)num3;
+        System.out.println(num4);
+        
+        //arithmetic operators
+        int number1 = 10;
+        int number2 = 2;
+        int sum = number1 + number2;
+        int sub = number1 - number2;
+        int mul = number1 * number2;
+        int div = number1 / number2;
+        int rem = number1 % number2;
+
+        System.out.println("Add: "+sum);
+        System.out.println("Subtract: "+sub);
+        System.out.println("Multiply: "+mul);
+        System.out.println("Divide: "+div);
+        System.out.println("Remain: "+rem);
+
+        */
+
+        //shortcuts to updating values
+        int x = 1;
+        x += 5;
+        System.out.println(x);
+        
+        int y = 2;
+        y-= 1;
+        System.out.println(y);
+
+        int z = 5;
+        z *= 5;
+
+        int c = 6;
+        c /= 2;
+
+        System.out.println(z);
+        System.out.println(c);
+
+
+
+    }
+}
